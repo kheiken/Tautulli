c647ae77716287b5dc3fe91cb750617ba92ca110
Karsten Heiken <karsten.heiken@gmail.com>
2024-10-20 22:51:38 +0000

    Add option to send Discord notifications silently
    Set the flags according to the Discord webhook documentation, so that the message does not ping messages on Discord:
    https://discord.com/developers/docs/resources/webhook#execute-webhook

--- plexpy/notifiers.py
+++ plexpy/notifiers.py
@@ -1121,6 +1121,7 @@ class DISCORD(Notifier):
                        'avatar_url': '',
                        'color': '',
                        'tts': 0,
+                       'silent_message': 0,
                        'incl_subject': 1,
                        'incl_card': 0,
                        'incl_description': 1,
@@ -1144,6 +1145,8 @@ class DISCORD(Notifier):
             data['avatar_url'] = self.config['avatar_url']
         if self.config['tts']:
             data['tts'] = True
+        if self.config['silent_message']:
+            data['flags'] = 2**12
 
         files = {}
 
@@ -1243,6 +1246,12 @@ class DISCORD(Notifier):
                           'value': self.config['tts'],
                           'name': 'discord_tts',
                           'description': 'Send the notification using text-to-speech.',
+                          'input_type': 'checkbox'
+                          },
+                         {'label': 'Silent message',
+                          'value': self.config['silent_message'],
+                          'name': 'discord_silent_message',
+                          'description': 'Send messages silently without any alert sounds.',
                           'input_type': 'checkbox'
                           },
                          {'label': 'Include Subject Line',
